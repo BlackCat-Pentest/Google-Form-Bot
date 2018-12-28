406b3427dad64815f4e202d61d99d13b5270dad4
Raphael Sander <raphael.sander75@gmail.com>
2018-12-28 20:53:11 -0200

    Implementação de PEP8.

--- main2.py
+++ main2.py
@@ -1,14 +1,15 @@
 from threading import Thread
 import os
 
+
 class Th(Thread):
 	def __init__(self, num):
 		Thread.__init__(self)
 		self.num = num
 		
 	def run(self):
-		 os.system("python hack.py")
-		 
+		os.system("python hack.py")
+
 
 for x in range(1, 20):
 	a = Th(x)
